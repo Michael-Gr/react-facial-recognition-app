b4d4aecbf24afc9a150d6c04ee93fbe886bab548
Michael Grein <michaelgrein92@gmail.com>
2019-07-09 11:35:13 -0400

    Formatting logo a bit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,9 @@ const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-				<div className="Tilt-inner"><img src={brain} alt='logo' /></div>
+				<div className="Tilt-inner pa3">
+					<img src={brain} alt='logo' style={{paddingTop: '10px'}} />
+				</div>
 			</Tilt>
 		</div>
 	);
